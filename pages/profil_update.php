f017765e3f36b23a934d9d673cbc3136221b9359
Splitziz <nudoru70@gmail.com>
2017-01-04 17:59:16 +0100

    Commencement du back office du profil (update)
    Je vous envoie la bdd par fb

---
+++ pages/profil_update.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once ("dao/DaoUtilisateur.php");
+
+
+$daoUtilisateur = new DaoUtilisateur();
+
+$id = $_GET['id'];
+$daoUtilisateur->find($id);
+
+$infos['oui'] = array();
+
+$infos['oui']['id']=$daoUtilisateur->bean->getId();
+$infos['oui']['nom']=$daoUtilisateur->bean->getNom();
+$infos['oui']['prenom']=$daoUtilisateur->bean->getPrenom();
+$infos['oui']['email']=$daoUtilisateur->bean->getEmail();
+$infos['oui']['description']=$daoUtilisateur->bean->getDescription();
+$infos['oui']['date_inscription']=$daoUtilisateur->bean->getDate_inscription();
+$infos['oui']['avatar']=$daoUtilisateur->bean->getImage();
+//        $daoUtilisateur->setLeAvatar();
+//        $infos['oui']['avatar']=$daoUtilisateur->bean->getLeAvatar()->getNom();
+
+$param=array("liste" =>$infos);
+
+//var_dump($param) or die();
+?>
